fd3d0ddbc411f04b8c3b3ccd86650768b28a0f0a
erenuzmez <91371455+erenuzmez@users.noreply.github.com>
2023-04-06 21:38:44 +0300

    Update Odev21UsluSayi.java

--- Odev21UsluSayi.java
+++ Odev21UsluSayi.java
@@ -2,14 +2,14 @@ package CDonguler;
 import java.util.Scanner;
 public class Odev21UsluSayi {
     public static void main(String[] args) {
-        int a,b,i,carpim=1;
+        int a,b,i,total=1;
         Scanner input=new Scanner(System.in);
         System.out.println("Üssü alınacak sayı:");
         a= input.nextInt();
         System.out.println("Üs olacak sayı:");
         b=input.nextInt();
         for(i=1;i<=b;i++){
-            carpim=carpim*a;
+            total=total*a;
         }
         System.out.println("Cevap:"+carpim);
     }
